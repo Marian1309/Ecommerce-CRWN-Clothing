484c842b38de1917e7f42839613371aa0dd52dd2
Marian <pidchashymaryan@gmail.com>
2022-07-26 14:27:57 +0300

    created auth component which has signup && signin components

--- src/modules/app/App.jsx
+++ src/modules/app/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { Home } from '../routes/home/Home';
 import { Navigation } from '../routes/navigation/Navigation';
-import { SignIn } from '../routes/sign-in/SignIn';
+import { Auth } from '../routes/auth/Auth';
 
 const Shop = () => {
   return (
@@ -21,7 +21,7 @@ export const App = () => {
           {/* index позначає домашню сторінку(основну) */}
           <Route index element={<Home />} />
           <Route path='shop' element={<Shop />} />
-          <Route path='sign-in' element={<SignIn />} />
+          <Route path='auth' element={<Auth />} />
         </Route>
       </Routes>
     </>
---
+++ src/modules/components/sign-in-form/SignInForm.jsx
@@ -0,0 +1,93 @@
+import { useState } from 'react';
+
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInUserWithEmailAndPassword,
+} from '../../utils/firebase/Firebase';
+import { Button } from '../button/Button';
+import { FormInput } from '../form-input/FormInput';
+
+import './SignInForm.scss';
+
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await signInUserWithEmailAndPassword(email, password);
+      console.log(response);
+      resetFormFields();
+    } catch (err) {
+      switch (err.code) {
+        case 'auth/user-not-found':
+          alert('No user associated with this account, please sign up');
+          break;
+        case 'auth/wrong-password':
+          alert('Incorrent password for email');
+          break;
+        case 'auth/weak-password':
+          alert('Length of the password need have at least 6 characters');
+          break;
+      }
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <div className='sign-in-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label='Email'
+          type='email'
+          required
+          name='email'
+          onChange={handleChange}
+          value={email}
+        />
+        <FormInput
+          label='Password'
+          type='password'
+          required
+          name='password'
+          onChange={handleChange}
+          value={password}
+        />
+
+        <div className='buttons-container'>
+          <Button type='submit'>Sign in</Button>
+          <Button onClick={signInWithGoogle} buttonType='google'>
+            Sign in with Google
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export { SignInForm };
